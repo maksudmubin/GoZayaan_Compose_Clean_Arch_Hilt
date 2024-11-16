4dcf65a361152d4b5d6298606076ba613a906977
Mubin <mubin@kolpolok.com>
2024-11-17 02:02:36 +0600

    # theme declared for compose environment

---
+++ app/src/main/java/com/mubin/gozayaan/base/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.mubin.gozayaan.base.theme
+
+import android.view.Surface
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xff1D2026)
+val Surface = Color(0xff2D303A)
